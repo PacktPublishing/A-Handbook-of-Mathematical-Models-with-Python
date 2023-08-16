6f08c4b1f0cfc9ee6c501ddd781098bdc0b76cff
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2023-08-17 00:51:42 +0530

    Update

--- chapters/CH3/lda.py
+++ chapters/CH3/lda.py
@@ -1,12 +1,9 @@
-
-#Author: ranja.sarkar@gmail.com
-
 from sklearn.datasets import make_classification
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-
+#dataset
 X, y = make_classification(n_samples = 1000, n_features = 8, n_informative = 8, n_redundant = 0, random_state = 1)
 
 model = LinearDiscriminantAnalysis()
@@ -17,10 +14,8 @@ grid['solver'] = ['svd', 'lsqr', 'eigen']
 
 search = GridSearchCV(model, grid, scoring = 'accuracy', cv = cv, n_jobs = -1)
 results = search.fit(X, y)
-print('Mean Accuracy: %.3f' % results.best_score_)
+#print('Mean Accuracy: %.3f' % results.best_score_)
 
-row = [0.1277, -3.6440, -2.2326, 1.8211, 1.7546, 0.1243, 1.0339, 2.3582] #new example
-yhat = search.predict([row]) #predict on test data
-print('Predicted Class: %d' % yhat) #class probability of new example
-
-
+row = [0.1277, -3.6440, -2.2326, 1.8211, 1.7546, 0.1243, 1.0339, 2.3582] #new example #test datapoint
+yhat = search.predict([row]) #predict
+print('Predicted Class: %d' % yhat) 
