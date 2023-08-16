f8f55b7a73e65b8d711e4f4cb11836c93fe94f9a
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2023-08-17 00:00:22 +0530

    Update

--- chapters/Part 3/CH9/classification_gridsearch.py
+++ chapters/Part 3/CH9/classification_gridsearch.py
@@ -3,29 +3,27 @@ import pandas as pd
 from scipy.stats import loguniform
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
 
-url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/sonar.csv'
-dataframe = pd.read_csv(url, header = None)
+#dataset
+dataframe = pd.read_csv('sonar.csv')
 data = dataframe.values
 X, y = data[:, :-1], data[:, -1]
-#print(X.shape, y.shape)
 
 #Model
 model = LogisticRegression()
 cv = RepeatedStratifiedKFold(n_splits = 10, n_repeats = 3, random_state = 1)
 
-#define search space
+#Define search space
 space = dict()
 space['solver'] = ['newton-cg', 'lbfgs', 'liblinear']
 space['penalty'] = ['none', 'l1', 'l2', 'elasticnet']
-space['C'] = loguniform(1e-5, 100)
-
-search = RandomizedSearchCV(model, space, n_iter = 500, scoring = 'accuracy', 
-                            n_jobs = -1, cv = cv, random_state = 1)
-
+space['C'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
+search = GridSearchCV(model, space, scoring = 'accuracy', n_jobs = -1, cv = cv)
 result = search.fit(X, y)
 
 print('Best Score: %s' % result.best_score_)
 print('Best Hyperparameters: %s' % result.best_params_)
+
+
 
