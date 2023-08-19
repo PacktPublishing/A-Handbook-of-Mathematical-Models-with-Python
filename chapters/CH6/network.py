20a1c2c4c25fa3731de07119525a008b4325ea0e
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2023-08-19 12:46:06 +0530

    Create

---
+++ chapters/CH6/network.py
@@ -0,0 +1,12 @@
+
+import pandas as pd, numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('FlightsUSA.csv')
+df = nx.from_pandas_edgelist(data, source = 'Origin', target = 'Dest', edge_attr = True)
+#df.nodes()
+#df.edges()
+
+plt.figure(figsize = (18,12))
+nx.draw_networkx(df, with_labels = True)
