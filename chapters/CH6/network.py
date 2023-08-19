5955796b38e779008ed87cb164b0f3777d19bbe1
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2023-08-19 12:49:54 +0530

    Update

--- chapters/CH6/network.py
+++ chapters/CH6/network.py
@@ -10,3 +10,7 @@ df = nx.from_pandas_edgelist(data, source = 'Origin', target = 'Dest', edge_attr
 
 plt.figure(figsize = (18,12))
 nx.draw_networkx(df, with_labels = True)
+
+shortest_airtime = nx.dijkstra_path(df, source = 'LAS', target = 'PBI', weight = 'AirTime')
+shortest_dist = nx.dijkstra_path(df, source = 'LAS', target = 'PBI', weight = 'Distance')
+print(shortest_dist,shortest_airtime)
