577adc974b9a404f390715555268d4f4aea628fd
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2024-09-12 10:49:04 +0530

    Update regression_gridsearch.py

--- Chapter09/regression_gridsearch.py
+++ Chapter09/regression_gridsearch.py
@@ -1,10 +1,15 @@
+#author: ranja.sarkar@gmail.com
 
 import pandas as pd
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import GridSearchCV
 
-#dataset
+import warnings
+warnings.filterwarnings("ignore")
+
+
+#dataset for regression
 url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/auto-insurance.csv'
 df = pd.read_csv(url, header = None)
 
@@ -18,13 +23,18 @@ cv = RepeatedKFold(n_splits = 10, n_repeats = 3, random_state = 1)
 
 #Define search space
 space = dict()
-space['solver'] = ['svd', 'cholesky', 'lsqr', 'sag']
 space['alpha'] = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100]
 space['fit_intercept'] = [True, False]
-space['normalize'] = [True, False]
+#space['normalize'] = [True, False]
+space['max_iter'] = [100, 200, 300]
+space['random_state'] = [1, 42]
+space['solver'] = ['svd', 'cholesky', 'lsqr', 'sag']
 
 search = GridSearchCV(model, space, scoring = 'neg_mean_absolute_error', n_jobs = -1, cv = cv)
 result = search.fit(X, y)
 
 print('Best Score: %s' % result.best_score_)
 print('Best Hyperparameters: %s' % result.best_params_)
+
+
+
