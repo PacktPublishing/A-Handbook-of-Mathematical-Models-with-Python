0ac80c7b8d522199eb130946bbb83e91960d8496
Ranja <101544669+ranja-sarkar@users.noreply.github.com>
2024-09-12 10:52:41 +0530

    Update

--- Chapter09/regression_randomsearch.py
+++ Chapter09/regression_randomsearch.py
@@ -1,3 +1,4 @@
+#author: ranja.sarkar@gmail.com
 
 import pandas as pd
 from scipy.stats import loguniform
@@ -5,7 +6,7 @@ from sklearn.linear_model import Ridge
 from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import RandomizedSearchCV
 
-#dataset
+#dataset for regression
 df = pd.read_csv('auto-insurance.csv')
 data = df.values
 X, y = data[:, :-1], data[:, -1]
@@ -20,7 +21,6 @@ space = dict()
 space['solver'] = ['svd', 'cholesky', 'lsqr', 'sag']
 space['alpha'] = loguniform(1e-5, 100)
 space['fit_intercept'] = [True, False]
-space['normalize'] = [True, False]
 
 search = RandomizedSearchCV(model, space, n_iter = 500, scoring = 'neg_mean_absolute_error', n_jobs = -1, cv = cv, random_state = 1)
 result = search.fit(X, y)
